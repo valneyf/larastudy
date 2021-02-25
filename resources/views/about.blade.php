4548db71c2a21feed10a5872b3a35cc15907c4ab
Valney Filho <valneyf.dev@gmail.com>
2021-02-25 12:58:41 -0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('title', 'About Us Page')
+
+@section('content')
+    <h1>About Us Page</h1>
+@endsection
---
+++ resources/views/services.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('title', 'Services Page')
+
+@section('content')
+    <h1>Services Page</h1>
+
+    <li>Service 1</li>
+    <li>Service 2</li>
+    <li>Service 3</li>
+@endsection
